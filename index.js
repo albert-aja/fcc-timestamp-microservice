84ae3f3dee977445fb9176b72e931fc35064940f
albert-aja <albertaries18@gmail.com>
2023-05-19 12:00:59 +0700

    FreeCodeCamp Timestamp Microservice

---
+++ index.js
@@ -0,0 +1,36 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+
+app.get("/api/:date?", (req, res) => {
+  let date = new Date();
+
+  if (req.params.date) {
+    let unixDate = +req.params.date;
+    date = isNaN(unixDate) ? new Date(req.params.date) : new Date(unixDate);
+
+    if (!(date instanceof Date) || isNaN(date.getTime()))
+      return res.json({ error: "Invalid Date" });
+  }
+
+  return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
